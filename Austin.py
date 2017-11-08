04033d8c74a1bd99189e50b6e76aa0139dd316ea
austinHuff <austin.huff25@gmail.com>
2017-11-08 10:37:45 -0500

    first commit

---
+++ Austin.py
@@ -0,0 +1,9 @@
+'''
+prompt user for seed(s) and add to URLs to-visit list 
+
+prompt user for keyword (may set default)
+
+default termination conditions = time limit OR page limit reached OR out of pages OR acquired sufficient jobs
+
+prompt user for termination conditions
+'''
