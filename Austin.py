b3314670a06cf0089ecbbb0839b5ee9f16043c2e
austinHuff <austin.huff25@gmail.com>
2017-11-08 11:13:14 -0500

    Nov 8

--- Austin.py
+++ Austin.py
@@ -7,3 +7,19 @@ default termination conditions = time limit OR page limit reached OR out of page
 
 prompt user for termination conditions
 '''
+visited = []
+seed = input("Enter starting URL: ")
+while seed != 'n':
+    if '.com' in seed or '.edu' in seed or '.gov' in seed or '.net' in seed or '.org' in seed:
+        # valid address
+        visited.append(seed)
+        seed = input("Enter another URL or n for next menu: ")
+    else:
+        # invalid address ending
+        seed = input("Enter URL that ends with .com, .edu, .gov, .net, .org: ")
+
+
+keyword = input("Enter keyword to search for or press enter for default: ")
+default = 'job' # change if needed
+if keyword = '':
+    keyword = default
