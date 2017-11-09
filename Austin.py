d88dc2fac6cc41d820debb1a51de2113155f1dd8
austinHuff <austin.huff25@gmail.com>
2017-11-08 20:29:30 -0500

    Nov 8

--- Austin.py
+++ Austin.py
@@ -7,19 +7,36 @@ default termination conditions = time limit OR page limit reached OR out of page
 
 prompt user for termination conditions
 '''
-visited = []
+toVisit = []
+valid = ['.com','.edu','.gov','.net','.org']
 seed = input("Enter starting URL: ")
 while seed != 'n':
     if '.com' in seed or '.edu' in seed or '.gov' in seed or '.net' in seed or '.org' in seed:
         # valid address
-        visited.append(seed)
+        toVisit.append(seed)
         seed = input("Enter another URL or n for next menu: ")
     else:
         # invalid address ending
-        seed = input("Enter URL that ends with .com, .edu, .gov, .net, .org: ")
+        seed = input("Enter URL that ends with .com, .edu, .gov, .net, .org or n for next menu: ")
 
 
 keyword = input("Enter keyword to search for or press enter for default: ")
 default = 'job' # change if needed
-if keyword = '':
+if keyword == '':
     keyword = default
+
+print(("\nChoose from the following termination conditions:"))
+print("1. Time limit\n2. Number of pages\n3. Until out of pages\n4. Collected a sufficient number of jobs")
+termCond = False
+while not termCond:
+    termination = input("Enter the number cooresponding to your termination choice: ") # or d for default?
+    if termination == '1':
+        # time limit
+    elif termination == '2':
+        # number of pages
+    elif termination == '3':
+        # until list is out of pages
+    elif termination == '4':
+        # number of jobs
+    else:
+        print("Error: Insert valid termination condition")
