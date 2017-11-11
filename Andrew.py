61136c5eed4ae49cef4e956f951db45ca2412ca6
Andrew Troyer <andrew.troyer@emu.edu>
2017-11-11 11:37:49 -0500

    Finished basic check_url()

--- Andrew.py
+++ Andrew.py
@@ -8,21 +8,34 @@ class URL:
     string address
 '''
 import time
+import url
 
-def check_url(to_visit, domain_times):
+def check_url(url, domain_times, visited):
     '''
     Check URL to make sure that it meets all of the criteria.
-    to_visit is a list of url objects
-    domain_times is a dict of domain name strings and floats of the time accessed
+    :param url: url to be checked
+    :param domain_times: a dict of domain name strings and floats of the time accessed
+    :param visited: a list of url strings that have already been visited
+    :return: the next valid url
     '''
-    ACCESS_TIME = 15 #Minimum number of seconds between website visits
-    
-    if len(to_visit) == 0:
-        raise ValueError
 
-    url = to_visit[0]
-    
-    #check if url.domain is in domain_times
-    while time.time() - url.time() <= ACCESS_TIME:
-        print('Waiting ', ACCESS_TIME - (time.time() - url.time()), ' seconds')
-        time.sleep(time.time()-url.time())
+    ACCESS_TIME = 15 #Minimum number of seconds between website pings
+
+    last_time = 0
+    if (url.domain in domain_times):
+        last_time = domain_times[url.domain]
+
+    if (url.address in visited):
+        return False
+
+    while (time.time() - last_time) <= ACCESS_TIME:
+        print('Waiting ', ACCESS_TIME - (time.time() - last_time), ' seconds for', url.domain)
+        time.sleep(ACCESS_TIME - (time.time()-last_time))
+
+    return True
+
+
+#Testing
+if __name__ == '__main__':
+    a = url.URL(domain = "www.facebook.com",address = "www.facebook.com/Andrew")
+    print(check_url(a,{"www.facebook.com":time.time()},['www.facebook.com/Joe Smith/profile']))
