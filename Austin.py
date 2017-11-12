d22bb8689893cc187b17256ee22ece3bfb098fff
austinHuff <austin.huff25@gmail.com>
2017-11-12 14:57:45 -0500

    added url objects to to_visit

--- Austin.py
+++ Austin.py
@@ -7,36 +7,50 @@ default termination conditions = time limit OR page limit reached OR out of page
 
 prompt user for termination conditions
 '''
+from url import *
 to_visit = []
-valid = ['.com','.edu','.gov','.net','.org']
+seeds = []
 seed = input("Enter starting URL: ")
 while seed != '':
-    if len(seed) > 5 and ('.com' in seed or '.edu' in seed or '.gov' in seed or '.net' in seed or '.org' in seed):
+    if len(seed) > 5 and not seed.startswith('.') and ('.com' in seed or '.edu' in seed or '.gov' in seed or '.net' in seed or '.org' in seed):
         # valid address
-        to_visit.append(seed)
-        seed = input("Enter  another URL or enter for next menu: ")
+        seeds.append(seed)
+        seed = input("Enter another URL or enter for next menu: ")
     else:
         # invalid address ending
-        seed = input("Enter URL that ends with .com, .edu, .gov, .net, .org or enter for next menu: ")
+        seed = input("Enter URL that ends with .com, .edu, .gov, .net, .org, not beginning with a '.' or enter for next menu: ")
 
 
 # get keyword(s)
 keyword = input("Enter keyword to search for or press enter for default: ")
 multiple = True
-keywords = []
+keywords = {}
 if keyword == '':
-    keywords.append('job') # change default if needed
+    keywords['job'] = 0 # change default if needed
     multiple = False
 else:
-    keywords.append(keyword)
+    keywords[keyword] = 0
     
 while multiple:
     keyword = input("Enter next keyword to search for or press enter for done: ")
     if keyword == '':
         multiple = False
     else:
-        keywords.append(keyword)
+        keywords[keyword] = 0
+
+######### create all URL objects here #########
+
+#what do i need to initialize date and domain to?        
+for i in range(len(seeds)):
+    i = URL(address=seeds[i],keywords=keywords)
+    to_visit.append(i)
+
+for i in to_visit:
+    print(i.toString())
+
     
+###############################################
+
 print(("\nChoose from the following termination conditions:"))
 print("1. Time limit\n2. Number of pages\n3. Until out of pages\n4. Collected a sufficient number of jobs")
 termCond = False
