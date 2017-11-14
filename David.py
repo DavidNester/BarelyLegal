8f57e9ab224ce1d65579a9788b3b80b4f62ff24b
DavidNester <14nesterd@gmail.com>
2017-11-14 12:01:18 -0500

    can fethc

--- David.py
+++ David.py
@@ -1,26 +1,17 @@
 import urllib.robotparser
-def checkRobots(url):
-	rp = robotparser.RobotFileParser()
-	rp.set_url(url+"/robots.txt")
-	rp.read()
-	if rp.canfetch("*",url):
-		return True
-	return False
-		#the following came from a stackexchange post and may be useful for 
-		"""
-		site = urllib.request.urlopen(url)
-		sauce = site.read()
-		soup = BeautifulSoup(sauce, "html.parser")
-		actual_url = site.geturl()[:site.geturl().rfind('/')]
-		my_list = soup.find_all("a", href=True)
-		for i in my_list:
-			# rather than != "#" you can control your list before loop over it
-			if i != "#":
-				newurl = str(actual_url+"/"+i)
-				try:
-					if rp.can_fetch("*", newurl):
-						site = urllib.request.urlopen(newurl)
-						# do what you want on each authorized webpage
-				except:
-					pass
-		"""
+from urlparse import urlparse
+
+
+def get_domain(url):
+	parsed_uri = urlparse(url)
+	domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+	return domain
+
+def can_visit(url):
+    domain = get_domain(url)
+    rp = robotparser.RobotFileParser()
+    rp.set_url(domain + "/robots.txt")
+    rp.read()
+    if rp.canfetch("*", url):
+        return True
+    
