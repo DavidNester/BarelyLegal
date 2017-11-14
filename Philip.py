39af3e4a9a4daa59bb9649acf7f6f3c980082a8d
Philip Park <parkphi56@gmail.com>
2017-11-14 10:13:26 -0500

    Update Philip.py
    really, I'm not sure how to do this part.

---
+++ Philip.py
@@ -0,0 +1,24 @@
+'''
+class URL:
+
+'''
+from Andrew.py import check_url
+import time
+import url
+
+def addit_url(url, visited):
+    '''
+    url: url to be checked
+    visited: visited strings
+    '''
+
+    if (url.address in check_url):
+        return false
+
+
+#Testing?
+if __name__ == '__main__':
+    a = url.URL(domain = "www.facebook.com",address = "www.facebook.com/Philip")
+    print(addit_url(a,{"www.facebook.com":},['www.facebook.com/Apple/index']))
+
+
