f25db85d0e0bf01695dd69c4779af08ed7f003d9
Andrew Troyer <andrew.troyer@emu.edu>
2017-11-16 11:44:19 -0500

    Changed url to be a object of a domain

--- url.py
+++ url.py
@@ -1,16 +1,12 @@
 class URL:
-	domain = str()
 	date = str()
 	keywords = dict()
-	visited = bool()
 	address = ""
 
-	def __init__(self, domain = "", date = "", keywords = {}, visited = False, address = ""):
-		self.domain = domain
+	def __init__(self, date = "", keywords = {}, address = ""):
 		self.date = date
 		self.keywords = keywords
-		self.visited = visited
 		self.address = address
 
 	def toString(self):
-		return [self.domain, self.date, self.keywords, self.visited, self.address]
+		return [self.date, self.keywords, self.address]
