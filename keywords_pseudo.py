825f7b5cb9c18739d9577b8f3090626cc3874461
FinnianW <finnian.wengerd@emu.edu>
2017-11-21 01:42:36 -0500

    Added functions part 2

--- keywords_pseudo.py
+++ keywords_pseudo.py
@@ -1,4 +1,8 @@
 import datetime
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
 #DO NOT IMPORT ALL
 #format time
 
@@ -22,24 +26,53 @@ Made:
 url_index --> count for index of to_visit[]
 user_keyword --> [word:#] for the keyword dictionary 
 '''
-for url_index in to_visit[] #url_index is the same as saying for i in...
+#for url_index in to_visit: #url_index is the same as saying for i in...
 #open domain
 #read file
-#this reads the html page with all the divs. We want the actual content of the page 
-    myFile = codecs.open(url_index, "r")
-    print (myFile.read())
-    url.index.visited = True
+#this reads the html page with all the divs. We want the actual content of the page
+
+url = (str domain, float time, string date, dict keywords("emu": 0), bool visited)
+
+keynum = 0    
+
+def keyword_search():
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    html = urlopen("https://www.emu.edu",context=ctx)
+
+    bsObj = BeautifulSoup(html.read(),"html.parser")
+    ##html = urlopen("file://C:/Users/carol/Desktop/Classes/Software Engineering/JobScraper/FinalProject/emu.html")
+    ##bsObj = BeautifulSoup(html.read())
+    bsObj = bsObj.get_text()
+    print(bsObj)
+    user_keyword = url.keyword[0]
+    global keynum
+    mywords = bsObj.split()
+    for word in mywords:
+        if keyword == word:
+            keynum +=1
+        else:
+            keynum +=0
+
+    print(keynum)
+    update_keynum()
+    update_date_time()
 
 
-#count keyword events 
-for user_keyword in url_index.keywords #user_keyword is the same as for i in...
-    for line in myFile:
-        if keyword in myFile:
-            keyword_count +=1
 
 #update url_index object
-if keyword in file
-    url_index.time = datetime.time #get time
-    url_index.date = datetime.date#get date
-    url_index.keywords[keyword] = keyword_count  #get count for each user_keyword
-    
+def update_url_values():
+    global keynum
+    if keynum !=0:
+        url.keyword[keyword] += keynum
+    url.time = datetime.time
+    url.date = datetme.date
+    url.visited = False
+    keynum = 0
+
+##if keyword in file:
+##    url_index.time = datetime.time #get time
+##    url_index.date = datetime.date#get date
+##    url_index.keywords[keyword] = keyword_count  #get count for each user_keyword
+##    
