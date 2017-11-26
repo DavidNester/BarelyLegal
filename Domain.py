2ce9cb466783453868cd7938539def3b86d3399f
Andrew Troyer <andrew.troyer@emu.edu>
2017-11-25 21:10:12 -0500

    Changing Domain to match new runner psuedo code Added an add_address function. Pulled functions from David.py to Domain.py

--- Domain.py
+++ Domain.py
@@ -1,14 +1,25 @@
 import time
 
+def get_domain(url):
+    """
+    gets domain of URL
+    :param url: url that we want to get domain from
+    :return: domain of url
+    """
+    parsed_uri = urlparse(url)
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    return domain
+
 class Domain:
     def __init__(self, domain):
-        self.domain = domain
+        self.domain = domain #The domain address (facebook.com)
         self.time = time.time()
         self.keywords = [] #list of strings
         self.urls_to_visit = []
         self.urls_visited = set()
         self.rp = robotparser.RobotFileParser()
-        self.rp.set_url(address + "/robots.txt")
+        #What happens if no robots.txt is found?
+        self.rp.set_url(domain + "/robots.txt")
         self.rp.read()
         self.wait_time = self.rp.crawl_delay("*")
         if self.wait_time is None:
@@ -16,12 +27,20 @@ class Domain:
 
     def add_address(self, address):
         '''
-        Checks if a url of the domain can be added to the list of urls
-        to visit.
-        param address:
-        :return:
+        Checks if a url of the domain can be added to
+        the list of urls to visit.
+        param address: the new address to be added
+        :return: true if the address is in the domain.
         '''
-        pass
+        if  get_domain(address) != self.domain:
+            return False
+        elif address in self.urls_visited:
+            return True
+        elif address in self.urls_to_visit:
+            return True
+        else:
+            self.urls_to_visit.append(address)
+        return True
 
     def can_visit(self, url):
         if self.rp.canfetch("*", url.address):
