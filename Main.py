b795b065e5dac4c1cca70a6d17a20cbe019c0882
Austin Huff <austin.huff25@gmail.com>
2017-11-28 16:41:24 -0500

    added user functionality to main

--- Main.py
+++ Main.py
@@ -28,3 +28,142 @@ class Scraper:
             # domain.visit_urls() visits all pages in domain and returns a list of new domains that are found
             # and a list of relevant urls
             new_domains,relevant_urls = domain.visit_urls()
+
+ALLOWED_DOMAINS = ['.com','.edu','.gov','.net','.org']
+
+def check_domain(address):
+    '''
+    checks given address to determine if it is valid
+    returns True or False
+    '''
+    for i in ALLOWED_DOMAINS:
+        if (str(i+'/') in address or address.endswith(i)) and len(address) >= 5:
+            return True
+    return False
+
+def check_termination(cond_num, current_val, termination_val):
+    '''
+    Checking if the termination conditions have been met
+    return True or False
+    '''
+    if cond_num == '1' or cond_num == '2' or cond_num == '4':
+        if current_val >= termination_val:
+            return False
+    elif cond_num == '3':
+        if current_val == termination_val:
+            # if to_visit list is empty
+            return False
+    return True
+
+def get_input(msg=''):
+    '''
+    runs input function
+    msg - message displayed to user
+    returns input function with message
+    '''
+    return input(msg)
+        
+def get_seeds():
+    '''
+    gets all seeds from user
+    returns list of seeds
+    ''' 
+    seeds = []
+    seed = get_input("Enter starting URL: ")
+    count = 0
+    while seed != '' or count == 0:
+        if check_domain(seed):
+            # valid address
+            seeds.append(seed)
+            seed = get_input("Enter another URL or enter for next menu: ")
+            count += 1
+        elif count == 0 and seed == '':
+            print("Error: Must have at least one seed")
+            seed = get_input("Enter starting URL: ")
+        else:
+            # invalid address ending
+            print("Enter URL that contains .com, .edu, .gov, .net, .org")
+            seed = get_input("Enter URL: ")
+    return seeds
+
+def get_keywords():
+    '''
+    gets keywords searching for from user
+    returns the list of keywords
+    ''' 
+    keyword = get_input("Enter keyword to search for or press enter for default: ")
+    multiple = True
+    keywords = []
+    if keyword == '':
+        keywords.append("jobs") # change default if needed
+        multiple = False
+    else:
+        keywords.append(keyword)
+        
+    while multiple:
+        keyword = get_input("Enter next keyword to search for or press enter for done: ")
+        if keyword == '':
+            multiple = False
+        else:
+            keywords.append(keyword)
+    return keywords
+
+def get_termination_conditions():
+    '''
+    gets termination conditions from the user
+    returns a list containing termination conditions & values
+    '''
+    print(("\nChoose from the following termination conditions:"))
+    print("1. Time limit\n2. Number of pages\n3. Until out of pages\n4. Collected a sufficient number of jobs")
+    termCond = False
+    while not termCond:
+        choice = get_input("Enter the number cooresponding to your termination choice: ") # or d for default?
+        if choice == '1':
+            # time limit
+            termCond = True
+            validCond = False
+            while not validCond:
+                try:
+                    timelimit = int(get_input("Enter the number of seconds you want the program to run for: "))
+                    validCond = True
+                    print("Termination conditions set")
+                except:
+                    print("Error: Please enter a valid number of seconds")
+            termination = ['1',timelimit]
+        elif choice == '2':
+            # number of pages
+            termCond = True
+            validCond = False
+            while not validCond:
+                try:
+                    pageslimit = int(get_input("Enter the number of pages you want the program to visit: "))
+                    validCond = True
+                    print("Termination conditions set")
+                except:
+                    print("Error: Please enter a valid number of pages")
+            termination = ['2',pageslimit]
+        elif choice == '3':
+            # until list is out of pages
+            termCond = True
+            termination = ['2',seeds]
+            print("Termination conditions set")
+        elif choice == '4':
+            # number of jobs
+            termCond = True
+            validCond = False
+            while not validCond:
+                try:
+                    jobslimit = int(get_input("Enter the number of jobs you want the program to collect: "))
+                    validCond = True
+                    print("Termination conditions set")
+                except:
+                    print("Error: Please enter a valid number of jobs")
+            termination = ['4',jobslimit]
+        else:
+            print("Error: Insert valid termination condition")
+    return termination
+
+if __name__ == "__main__":
+    seeds = get_seeds()
+    keywords = get_keywords()
+    termination_conditions = get_termination_conditions()
