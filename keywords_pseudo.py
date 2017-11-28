98aeb6e4f329284acebbecbb81118bf428bded1a
Finnian G Wengerd <finnian.wengerd@emu.edu>
2017-11-28 12:03:56 -0500

    add philips code

--- keywords_pseudo.py
+++ keywords_pseudo.py
@@ -3,11 +3,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 import url
-
+import re
 '''
 This code searches a page for instances of the user's keywords and creates a url object for each page with any of the keywords found.
 To run, this code needs one parameter "url" that is the address of each webpage.
 '''
+bsObj 
 keynum = 0
 keywords = ["jobs", "software"] # example keywords (these will be changed as the user specifies what they want)
 
@@ -27,30 +28,46 @@ def keyword_search(url):
     bsObj = BeautifulSoup(html.read(),"html.parser")
     bsObj = bsObj.get_text() 
     mywords = bsObj.split()
+    url_list = []
     for i in keywords:
         for word in mywords:
             if keyword == word:
                 keynum +=1
-            else:
-                keynum +=0
-
-        print(keynum)
-        create_url_values(url)
-
+        url_obj = create_url_values(url,keynum)
+        if not(url_obj is None):
+            url_list.append(url_obj)
+    return url_list
 
 
-def create_url_values(url):
+
+def create_url_values(url,keynum):
     '''
     creates a url object if a keyword is found at least once in page
     url - string of website address
     Returns url object
     '''
-    global keynum
-    if keynum !=0:
+    if keynum > 0:
         current_url = url()
         current_url.domain(url)
         current_url.date = datetime.date
         current_url.time = datetime.time
         current_url.keyword[keyword] += keynum
+        return current_url
+    else:
+        return None
         
-        keynum = 0
+        
+
+def collect_url(url):
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', bsObj)
+    return urls
+
+
+def parse_url(url, keywords):
+    '''
+    Returns url object and a list of other urls found
+    '''
+    url_list = keyword_search(url)
+    new_urls = collect_url(url)
+    
+    
