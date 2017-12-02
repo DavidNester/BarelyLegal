0a8c2e38ddcdc4d2663f5f649c25b0631bc9d664
austinHuff <austin.huff25@gmail.com>
2017-12-01 23:24:05 -0500

    Rough draft of GUI

--- WebCrawler_GUI.py
+++ WebCrawler_GUI.py
@@ -1,8 +1,10 @@
 from tkinter import *
-
+# http://usingpython.com/using-tkinter/
 url = ''
 seeds = []
 ALLOWED_DOMAINS = ['.com','.edu','.gov','.net','.org']
+keywords = []
+termination = []
 
 class Window(Frame):
 
@@ -17,45 +19,36 @@ class Window(Frame):
 
         self.master.title("WebCrawler")
         self.pack(fill=BOTH,expand=1)
-        self.configure(background="black")
-        #quitButton = Button(self,text='Quit', command=self.client_exit)
-        #quitButton.place(x=69, y=69)
+        #self.configure(background="black")
 
         lbl = Label(self, text = 'welcome label')
         lbl.pack()
         #lbl.place(x=150,y=5)
 
-        
-
         self.ent = Entry(self)
         self.ent.pack()
         #self.ent.place(x=150,y=25)
 
-        btn = Button(self, text='enter',command=self.seeds)
-        btn.pack()
+        self.btn_enter = Button(self, text='enter',command=self.get_seeds)
+        self.btn_enter.pack()
         #btn.place(x=150,y=45)
 
-        if len(seeds) != 0:
-            self.btn_next = Button(self, text='next',command=self.keywords)
-            self.btn_next.pack()
-
-        self.lbl_confirm = Label(self, text = 'enter seed(s) in box above')
-        self.lbl_confirm.pack()
-
+        self.btn_next = Button(self, text='next',command=self.keywords,state=DISABLED)
+        self.btn_next.pack()
         
-        # define menu
+        self.lbl_msg = Label(self, text = 'enter seed(s) in box above')
+        self.lbl_msg.pack()
+
+        # define dropdown menu
         menu = Menu(self.master)
         self.master.config(menu=menu)
 
-        #file button
+        #file button & exit
         file = Menu(menu)
         file.add_command(label='Exit',command=self.client_exit)
         menu.add_cascade(label='Menu',menu=file)
 
-##        #edit button
-##        edit = Menu(menu)
-##        edit.add_command(label='ShowText', command=self.showTxt)
-##        menu.add_cascade(label='Edit',menu=edit)
+
         
 
     def client_exit(self):
@@ -65,27 +58,138 @@ class Window(Frame):
         text = Label(self, text='Hello there')
         text.place(x=100,y=100)
 
-    def seeds(self):
+    def get_seeds(self):
         global url
         global seeds
-        url = self.ent.get()
+        url = self.ent.get().lower()
         self.ent.delete(0,'end')
         if check_domain(url):
-            self.lbl_confirm.config(text = url + ' added to seeds')# = Label(self, text = url + ' added')
-            self.lbl_confirm.pack()
+            self.lbl_msg.config(text = url + ' added to seeds\nAdd another seed or next to continue')
+            self.lbl_msg.pack()
             seeds.append(url)
-            self.btn_next = Button(self, text='next',command=self.keywords)
+            self.btn_next.config(text='next',command=self.keywords,state='normal')
             self.btn_next.pack()
         elif len(seeds) == 0 and url == '':
-            self.lbl_confirm.config(text = 'Error: must have at least one nonempty seed')# = Label(self, text = url + ' added')
-            self.lbl_confirm.pack()
+            self.lbl_msg.config(text = 'Error: must have at least one nonempty seed')
+            self.lbl_msg.pack()
         else:
-            self.lbl_confirm.config(text = "Enter URL that contains .com, .edu, .gov, .net, .org")# = Label(self, text = url + ' added')
-            self.lbl_confirm.pack()
-            
-    def keywords(self):
-        pass
+            self.lbl_msg.config(text = "Enter URL that contains .com, .edu, .gov, .net, .org")
+            self.lbl_msg.pack()
 
+    def get_keywords(self):
+        global keywords
+        
+        keyword = self.ent.get()
+        self.ent.delete(0,'end')
+
+        if keyword == '':
+            self.lbl_msg.config(text = 'Insert nonempty keyword value')
+        else:
+            self.btn_next.config(state='normal')
+            keywords.append(keyword)
+            self.lbl_msg.config(text = keyword + ' added to keywords\nAdd another keyword or next to continue')
+            self.lbl_msg.pack()
+            
+    def check_termination_value(self):
+        print("check_termination_value")
+        global termination
+
+        value = self.ent.get()
+        self.ent.delete(0,'end')
+        print(value)
+        if termination[-1] == '1':
+            try:
+                value = int(value)
+                termination.append(value)
+                self.done()
+            except:
+                self.lbl_msg.config(text = "Error: Invalid number of seconds\nEnter the number of seconds you want the program to run for ")
+                self.lbl_msg.pack()
+        elif termination[-1] == '2':
+            try:
+                value = int(value)
+                termination.append(value)
+                self.done()
+            except:
+                self.lbl_msg.config(text = "Error: Invalid number of pages\nEnter the number of seconds you want the program to run for ")
+                self.lbl_msg.pack()
+        elif termination[-1] == '4':
+            try:
+                value = int(value)
+                termination.append(value)
+                self.done()
+            except:
+                self.lbl_msg.config(text = "Error: Invalid number of jobs\nEnter the number of seconds you want the program to run for ")
+                self.lbl_msg.pack()
+        
+        
+    def get_termination_value(self):
+        print("get_termination_value")
+        global termination
+        self.btn_enter.config(command=self.check_termination_value)
+        self.btn_enter.pack()
+        if termination[-1] == '1':
+            self.lbl_msg.config(text = "Enter the number of seconds you want the program to run for ")
+            self.lbl_msg.pack()
+        elif termination[-1] == '2':
+            self.lbl_msg.config(text = "Enter the number of pages you want the program to run for ")
+            self.lbl_msg.pack()
+        elif termination[-1] == '4':
+            self.lbl_msg.config(text = "Enter the number of jobs you want the program to run for ")
+            self.lbl_msg.pack()
+        
+    def get_termination(self):
+        print("get_termination")
+        global termination
+        choice = self.ent.get()
+        self.ent.delete(0,'end')
+        print(choice)
+        if choice == '1' or choice == '2' or choice == '4':
+            # time limit or number of pages or number of jobs
+            termination.append(choice)
+            self.get_termination_value()
+        elif choice == '3':
+            # until list is out of pages
+            termination = ['3',seeds]
+            
+        else:
+            #print("Error: Insert valid termination condition")
+            self.lbl_msg.config(text = 'Error: Insert valid termination condition\nEnter the number cooresponding to your termination choice:\n1. Time limit\n2. Number of pages\n3. Until out of pages\n4. Collected a sufficient number of jobs')
+            self.lbl_msg.pack()
+        
+    def keywords(self):
+        print('in keywords')
+        self.btn_next.config(text='next',command=self.termination,state=DISABLED)
+        self.btn_next.pack()
+        self.lbl_msg.config(text = 'Insert keyword(s) to search for')# = Label(self, text = url + ' added')
+        self.lbl_msg.pack()
+        self.btn_enter.config(text='enter',command=self.get_keywords)
+        self.btn_enter.pack()
+
+
+    def termination(self):
+        print('in termination')
+        self.btn_next.pack_forget()
+        self.lbl_msg.config(text = 'Enter the number cooresponding to your termination choice:\n1. Time limit\n2. Number of pages\n3. Until out of pages\n4. Collected a sufficient number of jobs')
+        self.lbl_msg.pack()
+        
+        self.btn_enter.config(text='enter',command=self.get_termination)
+        self.btn_enter.pack()
+
+    def done(self):
+        print("start web scraping")
+        self.lbl_msg.pack()
+        self.btn_enter.config(state=DISABLED)
+        self.btn_enter.pack()
+        
+        self.destroy()
+
+        self.lbl_msg=Label(text = 'where are we putting the results?')
+        self.lbl_msg.place(x=50,y=50)
+        
+        self.lbl_brandon = Label(text='Brandon is in charge of making this look pretty')
+        self.lbl_brandon.place(x=100,y=100)
+        
 def check_domain(address):
     '''
     checks given address to determine if it is valid
