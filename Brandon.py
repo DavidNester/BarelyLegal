56fefee0a53839dc45b17ec55633b434de9f84a3
Brandon Chupp <brandon.chupp@emu.edu>
2017-12-04 08:41:44 -0500

    added csv and json log writing functions

--- Brandon.py
+++ Brandon.py
@@ -1,9 +1,11 @@
 import json
 import url
 import datetime
+import csv
 
 # Global variables to store log state
 MAX_WRITE_BUFFER = 5
+FILE_NAME = 'log.csv'
 data_to_write = {}
 
 
@@ -17,15 +19,36 @@ def append_to_log(url_object):
 	data_to_write[url_object.address] = [url_object.keywords, url_object.date]
 	if len(data_to_write) == MAX_WRITE_BUFFER:
 		_write_to_log(data_to_write)
+		data_to_write = {}
 
 def _write_to_log(data):
 	"""
-	Private function which writes to log.json
+	Private function which writes to the file
 	data - the dictionary to be written to the file
 	Returns void
 	"""
+	with open(FILE_NAME, 'a', newline='') as csvfile:
+	    csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+	    for entry in data:
+	    	csv_writer.writerow([entry, data[entry][0], data[entry][1]])
+
+	
+
+def convert_csv_to_json():
+	"""
+	Converts the log file into JSON format
+	Returns void
+	"""
+	result = {}
+	with open(FILE_NAME, 'r', newline='') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			print(row)
+			for entry_id in range(len(row)):
+				row[entry_id] = row[entry_id].replace("'", "\"")				
+			result[row[0]] = [json.loads(row[1])] + [row[2]]
 	with open('log.json', 'w+') as output_file:
-		json.dump(data, output_file)
+		json.dump(result, output_file)
 
 
 if __name__ == "__main__":
@@ -38,3 +61,4 @@ if __name__ == "__main__":
 	for i in range(len(urls_list)):
 		append_to_log(urls_list[i])
 	urls_list = []
+	convert_csv_to_json()
