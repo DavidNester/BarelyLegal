a931f0c1950c754d0546be774cb2268151965576
Finnian G Wengerd <finnian.wengerd@emu.edu>
2017-12-05 11:19:41 -0500

    Added Documentation

--- url.py
+++ url.py
@@ -3,17 +3,27 @@ import datetime
 
 
 class URL:
+
     date = str()
     keywords = dict()
     address = ""
 
     def __init__(self, date = "", keywords = {}, address = ""):
+        """
+        Initializes the URL object 
+        """
         self.date = date
         self.keywords = keywords
         self.address = address
 
     def __str__(self):
+        """
+        Returns the address of self
+        """
         return self.address
 
     def __eq__(self,other):
+        """
+        Returns Boolean if the address of self is the same as other
+        """
         return self.address == other.address
