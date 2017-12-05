c68e519e130b97c97634ccb1aa772f3285422aef
Finnian G Wengerd <finnian.wengerd@emu.edu>
2017-12-05 11:36:22 -0500

    Documentation Cont'

--- url.py
+++ url.py
@@ -10,7 +10,11 @@ class URL:
 
     def __init__(self, date = "", keywords = {}, address = ""):
         """
-        Initializes the URL object 
+        Initializes the URL object
+        :param self: = new object
+        :param date: = date visited
+        :param keywords: = user defined keywords to search
+        :prarm address: = the address of the new url object
         """
         self.date = date
         self.keywords = keywords
@@ -18,12 +22,17 @@ class URL:
 
     def __str__(self):
         """
+        Overwrites __str__ 
+        :param self: url object
         Returns the address of self
         """
         return self.address
 
     def __eq__(self,other):
         """
+        Overwrites __eq__ to compare self and other's addresses
+        :param self: First url
+        :param other: Second url
         Returns Boolean if the address of self is the same as other
         """
         return self.address == other.address
