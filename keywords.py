842e47371a26de507e456505974af88c0d789201
Andrew Troyer <andrew.troyer@emu.edu>
2017-12-06 14:18:48 -0500

    Changed keywords_psuedo to keywords.py. Also fixed typos

--- keywords.py
+++ keywords.py
@@ -4,13 +4,14 @@ from bs4 import BeautifulSoup
 import ssl
 import url
 import re
+
 """
 This code searches a page for instances of the user's keywords and creates a url object for each page with any of the keywords found.
 To run, this code needs one parameter "url" that is the address of each webpage.
 """
 
 
-def keyword_search(url, keywords):
+def keyword_search(address, keywords):
     """
     Checks for instances of keywords on page and finds other urls on page
     Returns whether keywords were found and list of urls found on page
@@ -19,17 +20,16 @@ def keyword_search(url, keywords):
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
     try:
-        html = urlopen(url, context=ctx)
+        html = urlopen(address, context=ctx)
     except:
         return False
-    
 
-    bsObj = BeautifulSoup(html.read(),"html.parser")
-    bsObj = bsObj.get_text().lower() 
+    bsObj = BeautifulSoup(html.read(), "html.parser")
+    bsObj = bsObj.get_text().lower()
     mywords = bsObj.split()
     url_list = collect_url(bsObj)
     keywords_found = False
-    keyword_count = {i:0 for i in keywords}
+    keyword_count = {i: 0 for i in keywords}
     print(keyword_count)
     for keyword in keywords:
         # Currently set to break the loop if any keyword is found and add the url to the list to be returned
@@ -38,8 +38,8 @@ def keyword_search(url, keywords):
                 keywords_found = True
                 keyword_count[keyword] += 1
                 break
-    
-    return URL(datetime.date, keywords_count, url), url_list
+
+    return url.URL(datetime.date, keyword_count, address), url_list
 
 
 def collect_url(bsObj):
