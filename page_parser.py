252d4fa847f55be5adea7563f43135736a84ecba
Andrew Troyer <andrew.troyer@emu.edu>
2017-12-07 12:00:50 -0500

    Fixed bug with not using the wait time.

--- page_parser.py
+++ page_parser.py
@@ -45,7 +45,6 @@ def keyword_search(address, keywords):
 
 
 def collect_url(bsObj):
-    print('here')
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', bsObj)
     print(urls)
     return urls
